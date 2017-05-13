2a3d54eb9858b69aa6c2adc7dedcd0ce59ade850
xpressmaya <happyera27@gmail.com>
2017-05-12 21:53:55 -0700

    adding a quick interests controller

--- app/controllers/v1/interests_controller.rb
+++ app/controllers/v1/interests_controller.rb
@@ -1,9 +1,25 @@
 module V1
   class InterestsController < ApplicationController
     def create
+      @user = User.find_by_id(params[:user_id])
+      @interest = Interest.new interest_params
+
+      if @interest.save
+        render json: @user, serializer: V1::UserSerializer, status: 200
+      else
+        render json: { error: t('interest_create_error')}, status: :unprocessable_entity
+      end
     end
 
     def destroy
+      @interest = Interest.find_by_id(params[:id])
+      @interest.destroy
+    end
+
+    private
+
+    def interest_params
+      params.require(:interest).permit(:description, :user_id)
     end
   end
 end
